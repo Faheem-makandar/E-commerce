5ecb5bd0c6eb6009c447f390877860e8f6c588bf
FaheemMakandar <faheemmakandar6@gmail.com>
2025-03-09 16:22:02 +0530

    add on file

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <div className='footer'>
       <div className="footer-logo">
         <img src={footer_logo} alt="" />
-        <p>SHOPPER</p>
+        <p>MF</p>
       </div>
       <ul className="footer-links">
         <li>Company</li>
@@ -33,7 +33,7 @@ const Footer = () => {
     </div>
     <div className="footer-copywrite">
         <hr />
-        <p>Copyight @ 2023 - All Right Reserved </p>
+        <p>Copyight @ 2025 - All Right Reserved </p>
     </div>
 </div>  )
 }
